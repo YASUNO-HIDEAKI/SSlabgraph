9aad74e78439dbba9371a641e590083c1835ebdb
YASUNO-HIDEAKI <hideaki.yasuno@outlook.com>
2023-06-03 14:29:34 +0900

    NMDS解析を追加した。Readmeファイルを今後追加していく。

--- R/ssl.NMDS.R
+++ R/ssl.NMDS.R
@@ -1,199 +1,222 @@
+#' @title NMDS analysis
+#' @description \code{f.NMDS.analysis} NMDS analysis
+#'
+#' @param data Amplicon or PLFA data
+#' @param dttype You choice "A" or "P". If you analyse amplicon data, you choice "A". If you analyse PLFA data, you choice "P".
+#' @param subsamplesize You enter sabsamplesize
+#' @param loc location data. You read location data sheet.
+#'
+#' @export
+#' @return data.score
+#' @return nmds.result
+#'
+#' @examples
+#' # f.NMDS.analysis(data = input, dytype = "A", subsamplesize = 20000, loc = loc)
 
 
-f.NMDS.dataset <-
-        function(data, dttype, subsamplesize) {
+f.NMDS.analysis <-
+        function(data, dttype, subsamplesize, loc) {
 
                 if(dtype == "P") {
 
                         input <- data
 
-                        id <-
-                                input %>%
+                        id <- input %>%
                                 rownames(.) %>%
                                 stringr::str_sub(., end = -2)
 
-                        id <-
-                                as.data.frame(id, row.names = names("id"))
+                        id <- as.data.frame(id, row.names = names("id"))
 
-                        input2 <-
-                                cbind(input, id)
+                        input2 <- cbind(input, id)
 
                         input2 <<- input2
 
                 }else {
 
-                        input <-
-                                data %>%
-                                t()
+                        input <- data %>%
+                                  t()
 
-                        input.excel.5OTU <-
-                                subset(input, rowSums(input) > 5) %>%
-                                t()
+                        input.excel.5OTU <- subset(input, rowSums(input) > 5) %>%
+                                             t()
 
-                        input.cleaned <-
-                                subset(input.excel.5OTU, rowSums(input.excel.5OTU) > 5) %>%
-                                t()
+                        input.cleaned <- subset(input.excel.5OTU, rowSums(input.excel.5OTU) > 5) %>%
+                                          t()
 
                         subsamplesize <- subsamplesize
 
-                        tobe.rarefied <-
-                                subset(input.cleaned, rowSums(input.cleaned) >= subsamplesize)
+                        tobe.rarefied <- subset(input.cleaned, rowSums(input.cleaned) >= subsamplesize)
 
-                        rarefied <-
-                                rrarefy(tobe.rarefied, subsamplesize)
+                        rarefied <- rrarefy(tobe.rarefied, subsamplesize)
 
-                        mean <-
-                                rowSums(rarefied) %>%
-                                mean()
+                        mean <- rowSums(rarefied) %>%
+                                 mean()
 
-                        min <-
-                                rowSums(rarefied) %>%
+                        min <- rowSums(rarefied) %>%
                                 min()
 
-                        input <-
-                                subset(t(rarefied), rowSums(t(rarefied)) > 0) %>%
-                                t()
+                        input <- subset(t(rarefied), rowSums(t(rarefied)) > 0) %>%
+                                  t()
 
-                        id <-
-                                input %>%
+                        id <- input %>%
                                 rownames(.) %>%
                                 stringr::str_sub(., end = -2)
 
-                        input2 <-
-                                cbind(input, id)
+                        input2 <- cbind(input, id)
 
                         input2 <<- input2
                         print(mean)
                         print(min)
 
-                }
-        }
+                beta.dist <- vegdist(input2, method = "bray")
 
-f.NMDS.analysis <-
-        function(input2, loc) {
+                nmds.result <- metaMDS(input2, distance = "bray")
+                nmds.result <<- nmds.result
 
-                beta.dist <-
-                        vegdist(input, method = "bray")
-
-                nmds.result <-
-                        metaMDS(input, distance = "bray")
                 print(nmds.result)
 
                 loc <- loc
 
-                perANOVA <-
-                        adonis(beta.dist ~ loc$id,
-                               permutations = 10000)
+                perANOVA <-adonis(beta.dist ~ loc$id, permutations = 10000)
 
                 print(perANOVA)
 
-                data.score <- 　　　#nmds.resultからNMDSのX軸Y軸の値を取得
-                        nmds.result %>%
-                        scores() %>%
-                        as.data.frame()
+                data.score <- nmds.result %>%
+                               scores() %>%
+                               as.data.frame()
 
-                data.score <-
-                        cbind(data.score, loc)
+                data.score <- cbind(data.score, loc)
 
                 data.score <<- data.score
 
+                }
+
         }
 
-f.envfit.analysis <-
-        function(nmds.result, env) {
+#' @title Envfit analysis
+#' @description \code{f.envfit.analysis} Envfit analysis
+#'
+#' @param data Enter the "nmds.result" obtained from "f.NMDS.analysis
+#' @param env Environment data. You read Environmental data sheet.
+#'
+#' @export
+#' @return env.vec
+#'
+#' @examples
+#' # environment <- read.csv(environment.csv, header = T, stringAsFactor = T)
+#' #f.envfit.analysis(data = nmds.resuslt, env = environment)
+#'
 
+f.envfit.analysis <-
+        function(data, env) {
+
+                nmds.result <- data
                 env <- env
 
-                env.fit <-
-                        envfit(nmds.result, env, permu = 10000)
+                env.fit <- envfit(nmds.result, env, permu = 10000)
 
-                env.vec <-
-                        scores(env.fit, display = "vectors") %>%
-                        as.data.frame()
+                env.vec <- scores(env.fit, display = "vectors") %>%
+                            as.data.frame()
 
-                env.vec <-
-                        cbind(env.vec, vecname = rownames(env.vec))
+                env.vec <- cbind(env.vec, vecname = rownames(env.vec))
 
                 env.vec <<- env.vec
 
         }
 
-NMDS.graph <-
-        function(data, env) {
+#' @title NMDS graph
+#' @description \code{f.NMDS.graph} create NMDS plot
+#'
+#' @param data Enter the "data.score" obtained from "f.nmds.result"
+#' @param env Enter the "env.vec" obtained from "f.envfit.analysis". You can omit.
+#' @param color Enter the color name or hexadecimal color code. You can omit.
+#' @param shape Enter the shape number. You can omit.
+#'
+#' @export
+#' @return NMDS.graph
+#'
+#' @examples
+#' # NMDS.graph(data = data.score)
+#' # NMDS.graph(data = data.score, color = c("#0000ff", "#ffa500), shape = c(1, 2))
+#' # NMDS.graph(data = data.score, env = env.vec)
+#'
+
+f.NMDS.graph <-
+        function(data, env, color, shape) {
+
+                graph <-
+                        data %>%
+                        ggplot(aes(x = NMDS1, y = NMDS2, color = id, shape = id)) +
+                        geom_hline(yintercept = 0,
+                                   linetype = "dashed",
+                                   size = 0.5,
+                                   color = "black") +
+                        geom_vline(xintercept = 0,
+                                   linetype = "dashed",
+                                   size = 0.5,
+                                   color = "black") +
+                        geom_point(stat = "identity",
+                                   size = 7,
+                                   stroke = 3) +
+                        theme_test(base_rect_size = 1.5,　
+                                   base_line_size = 1) +
+                        theme(legend.text = element_text(size = 24),　
+                              legend.key.size = unit(1.2, "cm"),　#凡例の大きさ
+                              legend.title = element_text(size = 20), #凡例タイトルの大きさ
+                              axis.title = element_text(size = 24),　#軸タイトルの大きさ
+                              axis.text = element_text(size = 24, color = "black"),　#軸ラベルの大きさ
+                              plot.margin = unit(c(1,1,1,1), "lines"))
 
                 if(missing(env)) {
 
-                        NMDS.graph <-
-                                data %>%
-                                ggplot(.,
-                                       aes(x = NMDS1,
-                                           y = NMDS2,
-                                           color = id,
-                                           shape = id)) +
-                                geom_hline(yintercept = 0,
-                                           linetype = "dashed",
-                                           size = 0.5,
-                                           color = "black") +
-                                geom_vline(xintercept = 0,
-                                           linetype = "dashed",
-                                           size = 0.5,
-                                           color = "black") +
-                                geom_point(stat = "identity",
-                                           size = 7, 　#プロットの大きさ
-                                           stroke = 3) +  #プロットの枠線の太さ　
-                                theme_test(base_rect_size = 1.5,　#グラフスタイル
-                                           base_line_size = 1) +
-                                theme(legend.text = element_text(size = 24),　#凡例の文字サイズ
-                                      legend.key.size = unit(1.2, "cm"),　#凡例の大きさ
-                                      legend.title = element_text(size = 20), #凡例タイトルの大きさ
-                                      axis.title = element_text(size = 24),　#軸タイトルの大きさ
-                                      axis.text = element_text(size = 24, color = "black"),　#軸ラベルの大きさ
-                                      plot.margin = unit(c(1,1,1,1), "lines")) #図の余白
-
-                        NMDS.graph <<- NMDS.graph
-                        print(NMDS.graph)
-
                 }else{
 
-                        NMDS.graph.env <-
-                                data %>%
-                                ggplot(.,
-                                       aes(x = NMDS1,
-                                           y = NMDS2,
-                                           color = id,
-                                           shape = id)) +
-                                geom_hline(yintercept = 0,
-                                           linetype = "dashed",
-                                           size = 0.5,
-                                           color = "black") +
-                                geom_vline(xintercept = 0,
-                                           linetype = "dashed",
-                                           size = 0.5,
-                                           color = "black") +
-                                geom_point(stat = "identity",
-                                           size = 7, 　#プロットの大きさ
-                                           stroke = 3) +  #プロットの枠線の太さ　
-                                geom_segment(data = env.vec,
+                        graph <-
+                                graph +
+                                geom_segment(data = env,
                                              aes(x = 0, xend = NMDS1, y = 0, yend = NMDS2),
                                              arrow = arrow(length = unit(0.25, "cm")),
                                              color = "black") +
-                                geom_text(data = env.vec,
+                                geom_text(data = env,
                                           aes(x = NMDS1,
                                               y = NMDS2,
                                               label = vecname),
-                                          size = 3) +
-                                theme_test(base_rect_size = 1.5,　#グラフスタイル
-                                           base_line_size = 1) +
-                                theme(legend.text = element_text(size = 24),　#凡例の文字サイズ
-                                      legend.key.size = unit(1.2, "cm"),　#凡例の大きさ
-                                      legend.title = element_text(size = 20), #凡例タイトルの大きさ
-                                      axis.title = element_text(size = 24),　#軸タイトルの大きさ
-                                      axis.text = element_text(size = 24, color = "black"),　#軸ラベルの大きさ
-                                      plot.margin = unit(c(1,1,1,1), "lines"))
+                                          size = 3)
 
-                        NMDS.graph.env <<- NMDS.graph.env
-                        print(NMDS.graph.env)
 
+                }
+
+                if(missing(color) & missing(shape)) {
+
+                        NMDS.graph <<- graph
+                        print(graph)
+
+                }else if(missing(color)) {
+
+                        graph <-
+                                graph +
+                                scale_shape_manual(values = shape)
+
+                        NMDS.graph <<- graph
+                        print(graph)
+
+                }else if(missing(shape)) {
+
+                        graph <-
+                                graph +
+                                scale_color_manual(values = color)
+
+                        NMDS.graph <<- graph
+                        print(graph)
+
+                }else{
+
+                        graph <-
+                                graph +
+                                scale_shape_manual(value = shape) +
+                                scale_color_manual(value = color)
+
+                        NMDS.graph <<- graph
+                        print(graph)
 
                 }
         }
--- README.md
+++ README.md
@@ -40,6 +40,35 @@ SSlabgraph::boxplot(data = data, x = X, y = Y, ylab = "Y", xlab = "X", fill = X,
 SSlabgraph::boxplot(data = data, x = X, y = Y, ylab = "Y", xlab = "X", fill = X, col = c("#006400", "#ffff00"))
 ```
 
+### NMDS
+You can analyse NMDS and create NMDS plot easily.
+First, You read the amplicon or PLFA data sheet and location data sheet.
+
+``` r
+data <- read.csv("amplicon.csv", header = T, stringsAsFactors = T)
+loc <- read.csv("loc.csv", headet = T, stringsAsFactors = T)
+```
+Second, you ran "f.NMDS.analysis"
+The variables are "data", "dttype", "subsamplesize" and "loc". 
+The "data" is enter the amplicon or PLFA data.
+The "dttype" is enter "A" or "P". When you analyse amplicon data, you enter "A". When you analyse PLFA data, you enter "P".
+The "subsamplesize" is enter any number.
+The "loc" is enter location data. 
+
+``` r
+SSlabgraph::f.NMDS.analysi(data = data, dttype = "A", subsamplesie = 20000, loc = loc)
+```
+
+Third, if you want, you ran "f.enfit.analysis".
+The variables are "data" and "env"
+The "data" is the "nmds.result that is result of f.NMDS.result.
+The "env" is environment data.
+
+``` r
+# env <- read.csv("environment.csv", header = T, stringsAsFactors = T)
+SSlabgraph::f.env.analysi(data = nmds.resut, env = env)
+```
+
 ## Example
 
 This is a basic example which shows you how to solve a common problem:
